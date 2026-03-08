c6757b0b208ae7b8f0989fef8f762f833f188e59
basudevkumar <jhulonkumar40@gmail.com>
2026-03-09 02:19:35 +0600

    fixed some problems in react router

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="h-screen flex items-center justify-center  bg-gray-300 ">
-      <h1 className="text-[65px] font-bold text-white">
-        Wellcome to Home page
-      </h1>
+      <div>
+        <h1 className="text-[65px] font-bold text-white">
+          Wellcome to Home page
+        </h1>
+
+        <button className="py-7">
+          <Link
+            className="py-2 px-4 border rounded-md text-2xl font-medium"
+            to={"/shop"}
+          >
+            Go to Shop
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
--- src/page/Shop.jsx
+++ src/page/Shop.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Shop = () => {
   return (
     <div className="h-screen flex items-center justify-center  bg-gray-300 ">
-      <h1 className="text-[65px] font-bold text-white">
-        Wellcome to Shop page
-      </h1>
+      <div>
+        <h1 className="text-[65px] font-bold text-white">
+          Wellcome to Shop page
+        </h1>
+
+        <button className="py-7">
+          <Link
+            className="py-2 px-4 border rounded-md text-2xl font-medium"
+            to={"/"}
+          >
+            Back Home
+          </Link>
+        </button>
+      </div>
     </div>
   );
 };
