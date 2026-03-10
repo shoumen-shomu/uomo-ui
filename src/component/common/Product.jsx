b1070c78541781649163587c40dd33ef33cf3d28
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-10 17:02:32 +0600

    Products Part all Done

--- src/component/common/Product.jsx
+++ src/component/common/Product.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Images from "./Images";
-import product from "@/assets/images/product.png";
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa6";
 
-const Product = ({imgSrc,imgAlt,catagory,itemName,itemPrice}) => {
+const Product = ({
+  imgSrc,
+  imgAlt,
+  catagory,
+  itemName,
+  itemPrice,
+  discountPrice,
+}) => {
+  const [isLiked, setIsLiked] = useState(false);
   return (
     <>
       <div className="w-82.5">
         <Images className={""} imgSrc={imgSrc} imgAlt={imgAlt} />
         <div className="mt-3.5">
-         <div className="flex justify-between">
-           <p className="texts_14_regular text-[#767676]">{catagory}</p>
-           <FaRegHeart  className="text-[#767676] cursor-pointer" size={14} />
-         </div>
-         <p className="texts_16_regular text-head pt-0.5">{itemName}</p>
-         <p className="texts_16_regular text-head">{itemPrice}</p>
+          <div className="flex justify-between items-center">
+            <p className="texts_14_regular text-[#767676]">{catagory}</p>
+
+            <div onClick={()=>setIsLiked(!isLiked)} className="cursor-pointer">
+              {isLiked ? (
+                <FaHeart  className="text-red cursor-pointer" size={14} />
+              ) : (
+                <FaHeart 
+                  className="text-[#767676] cursor-pointer"
+                  size={14}
+                />
+              )}
+            </div>
+          </div>
+          <p className="texts_16_regular text-head pt-0.5">{itemName}</p>
+          <div className="flex gap-2 items-center">
+            {discountPrice ? (
+              <>
+                <p className="texts_16_regular text-[#767676] line-through">
+                  ${itemPrice}
+                </p>
+                <p className="texts_16_regular text-red font-bold">
+                  ${discountPrice}
+                </p>
+              </>
+            ) : (
+              <p className="texts_16_regular text-head">${itemPrice}</p>
+            )}
+          </div>
         </div>
       </div>
     </>
--- src/component/layouts/home/Products.jsx
+++ src/component/layouts/home/Products.jsx
@@ -1,9 +1,24 @@
 import Product from "@/component/common/Product";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import product from "@/assets/images/product.png";
+import mixitup from "mixitup";
 
 const Products = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      mixitup(containerRef.current, {
+        animation: {
+          duration: 400,
+        },
+        selectors: {
+          target: ".mix",
+        },
+      });
+    }
+  }, []);
   return (
     <>
       <section className="mt-23.5">
@@ -15,84 +30,117 @@ const Products = () => {
             </h2>
             <div className="flex justify-center items-center gap-10 mt-7.75">
               <Link>
-                <p className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]">
+                <p
+                  data-filter=".all"
+                  className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]"
+                >
                   ALL
                 </p>
               </Link>
               <Link>
-                <p className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]">
+                <p
+                  data-filter=".newArrivals"
+                  className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]"
+                >
                   NEWARRIVALS
                 </p>
               </Link>
               <Link>
-                <p className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]">
+                <p
+                  data-filter=".bestSeller"
+                  className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]"
+                >
                   BEST SELLER
                 </p>
               </Link>
               <Link>
-                <p className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]">
+                <p
+                  data-filter=".toprating"
+                  className="texts_16_medium text-[#767676] hover:text-head duration-500 relative after:content-[''] after:absolute after:bottom-0 after:left-0  after:w-0 after:h-0.5 after:bg-head after:transition-all after:duration-400 hover:after:w-[60%]"
+                >
                   TOP RATING
                 </p>
               </Link>
             </div>
           </div>
-          <div className="mt-5.5 grid grid-cols-4 gap-7.5">
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
-            <Product
-              imgSrc={product}
-              imgAlt={product}
-              catagory={"Dresses"}
-              itemName={"Cropped Faux Leather Jacket"}
-              itemPrice={"$29"}
-            />
+          <div
+            ref={containerRef}
+            className="mt-5.5 flex flex-wrap justify-center gap-7.5 justify-center"
+          >
+            <div className="mix all newArrivals bestSeller">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cropped Faux Leather Jacket"}
+                itemPrice={"29"}
+              />
+            </div>
+            <div className="mix all newArrivals bestSeller">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Calvin Shorts"}
+                itemPrice={"62"}
+              />
+            </div>
+            <div className="mix all  ">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"CKirby T-Shirt"}
+                itemPrice={"17"}
+              />
+            </div>
+            <div className="mix all  toprating">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cableknit Shawl"}
+                itemPrice={"129"}
+                discountPrice={"99"}
+              />
+            </div>
+            <div className="mix all  ">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cropped Faux Leather Jacket"}
+                itemPrice={"29"}
+              />
+            </div>
+            <div className="mix all newArrivals toprating">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cropped Faux Leather Jacket"}
+                itemPrice={"29"}
+              />
+            </div>
+            <div className="mix all newArrivals ">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cropped Faux Leather Jacket"}
+                itemPrice={"29"}
+              />
+            </div>
+            <div className="mix all  toprating">
+              <Product
+                imgSrc={product}
+                imgAlt={product}
+                catagory={"Dresses"}
+                itemName={"Cropped Faux Leather Jacket"}
+                itemPrice={"129"}
+                discountPrice={"99"}
+              />
+            </div>
           </div>
         </div>
       </section>
