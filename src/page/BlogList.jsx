e2eb984eaba0d0305874dc29bd6d0698d1975ba3
shoumen-shomu <shoumenshomu@gmail.com>
2026-03-13 02:20:50 +0600

    Add:Blog Mixitup

--- src/page/BlogList.jsx
+++ src/page/BlogList.jsx
@@ -1,15 +1,31 @@
 import Blog from "@/component/common/Blog";
 import Container from "@/component/common/Container";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import blogImg from "@/assets/images/blogImg.png";
+import mixitup from "mixitup";
 
 const BlogList = () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      mixitup(containerRef.current, {
+        animation: {
+          duration: 400,
+        },
+        selectors: {
+          target: ".mix",
+        },
+      });
+    }
+  }, []);
+
   return (
     <section>
       <Container>
         <h2 className="head_35_bold text-head pb-2.75">The Blog</h2>
-        <div className="flex gap-10">
+        <div className="flex gap-10 pb-12.5">
           <Link>
             <p
               data-filter=".all"
@@ -59,47 +75,55 @@ const BlogList = () => {
             </p>
           </Link>
         </div>
-        <div className="grid grid-cols-2 gap-7.5">
-          <Blog
-            imgSrc={blogImg}
-            imgAlt={blogImg}
-            author={"BY ADMIN"}
-            date={"APRIL 05, 2020"}
-            blogname={"Woman with good shoes is never be ugly place"}
-            blogdescription={
-              "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
-            }
-          />
-          <Blog
-            imgSrc={blogImg}
-            imgAlt={blogImg}
-            author={"BY ADMIN"}
-            date={"APRIL 05, 2020"}
-            blogname={"Woman with good shoes is never be ugly place"}
-            blogdescription={
-              "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
-            }
-          />
-          <Blog
-            imgSrc={blogImg}
-            imgAlt={blogImg}
-            author={"BY ADMIN"}
-            date={"APRIL 05, 2020"}
-            blogname={"Woman with good shoes is never be ugly place"}
-            blogdescription={
-              "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
-            }
-          />
-          <Blog
-            imgSrc={blogImg}
-            imgAlt={blogImg}
-            author={"BY ADMIN"}
-            date={"APRIL 05, 2020"}
-            blogname={"Woman with good shoes is never be ugly place"}
-            blogdescription={
-              "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
-            }
-          />
+        <div ref={containerRef} className="flex flex-wrap gap-7.5">
+          <div className="mix all company beauty">
+            <Blog
+              imgSrc={blogImg}
+              imgAlt={blogImg}
+              author={"BY ADMIN"}
+              date={"APRIL 05, 2020"}
+              blogname={"Woman with good shoes is never be ugly place"}
+              blogdescription={
+                "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
+              }
+            />
+          </div>
+          <div className="mix all company style">
+            <Blog
+              imgSrc={blogImg}
+              imgAlt={blogImg}
+              author={"BY ADMIN"}
+              date={"APRIL 05, 2020"}
+              blogname={"Woman with good shoes is never be ugly place"}
+              blogdescription={
+                "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
+              }
+            />
+          </div>
+          <div className="mix all trends fashion">
+            <Blog
+              imgSrc={blogImg}
+              imgAlt={blogImg}
+              author={"BY ADMIN"}
+              date={"APRIL 05, 2020"}
+              blogname={"Woman with good shoes is never be ugly place"}
+              blogdescription={
+                "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
+              }
+            />
+          </div>
+          <div className="mix all style trends">
+            <Blog
+              imgSrc={blogImg}
+              imgAlt={blogImg}
+              author={"BY ADMIN"}
+              date={"APRIL 05, 2020"}
+              blogname={"Woman with good shoes is never be ugly place"}
+              blogdescription={
+                "Midst one brought greater also morning green saying had good. Open stars day let over gathered, grass face one every light of under."
+              }
+            />
+          </div>
         </div>
       </Container>
     </section>
