887be7cafdf78fd3d66a90fda49b6a0e2a389a8b
jihadcodes <jihad.dev.pro@gmail.com>
2026-03-12 13:29:01 +0600

    order page

--- src/component/mainDashboard/dashborarAllComponent/DashboardAddress.jsx
+++ src/component/mainDashboard/dashborarAllComponent/DashboardAddress.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const DashboardAddress = () => {
   return (
-    <div>DashboardAddress</div>
+    <section className='mt-25 ml-5 bg-amber-100'>
+     <div className='w-216.75'>
+       <p className='texts_14_regular text-head mb-20'>The following addresses will be used on the checkout page by default.</p>
+
+       <div className="flex justify-between">
+        <div className="add_left"></div>
+        <div className="add_right"></div>
+       </div>
+     </div>
+    </section>
   )
 }
 
