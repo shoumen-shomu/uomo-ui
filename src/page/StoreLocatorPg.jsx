c86b66e1bc9ac21754e8f181a3d06e3daf32278b
basudevkumar <jhulonkumar40@gmail.com>
2026-03-18 20:28:24 +0600

    fix: in src/component/storeLocator/Store_location.jsx compomnents added some responsive

---
+++ src/page/StoreLocatorPg.jsx
@@ -0,0 +1,12 @@
+import Store_location from '@/component/storeLocator/Store_location'
+import React from 'react'
+
+const StoreLocatorPg = () => {
+  return (
+    <>
+      <Store_location/>
+    </>
+  )
+}
+
+export default StoreLocatorPg
